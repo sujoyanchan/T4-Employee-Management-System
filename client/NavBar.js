590fcc4a6e10f73715f577bbd83b3679eb2d072e
Sujoy Anchan <sujoy259@gmail.com>
2024-03-10 11:00:55 +0530

    Update NavBar.js

--- client/NavBar.js
+++ client/NavBar.js
@@ -1,6 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function NavBar() {
+
+    const location = useLocation();
+    const isHomePage = location.pathname === '/';
+    if (isHomePage) {
+        return null;
+    }
 
     return (
         <>
