c08cd92a57321dd1655285abf3f6cea75157c5a4
Sujoy Anchan <sujoy259@gmail.com>
2024-03-10 11:01:31 +0530

    Update App.js

--- client/App.js
+++ client/App.js
@@ -5,21 +5,21 @@ import View from './View';
 import Update from './Update';
 import Delete from './Delete';
 import NavBar from './NavBar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        {window.location.pathname !== '/' && <NavBar />}
+        <NavBar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/add' element={<Add />} />
           <Route path='/view' element={<View />} />
           <Route path='/update' element={<Update />} />
           <Route path='/delete' element={<Delete />} />
-          <Route path='*' element={<Home />} />
+          <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </BrowserRouter>
     </>
